6a834dda313067fc643932e7e0b0f7d5841bfc82
mizzu-creations <mizzu.creations@gmail.com>
2024-02-29 02:59:15 +0900

    Feat : 검색 input radio (2) - 탭에 따라 다른 input 타이틀 노출 구현

--- headerSearch.js
+++ headerSearch.js
@@ -22,8 +22,12 @@ const selectSearchTab = () => {
   const radio2 = document.querySelectorAll("#searchChoice2");
   const radio2Label = document.querySelector(`label[for='searchChoice2']`);
   const contentSelects = document.querySelectorAll(".search__content__select");
+  const selectLabel = document.querySelector(".select-label");
+  const selectedOption = document.querySelector(".label");
 
   const labelArr = ["사이즈로", "규격", "브랜드"];
+  const labelName = ["타이어 사이즈", "델코 배터리 규격", "패키지 브랜드"];
+  const selected = ["인치", "전체", "전체"];
 
   const activateTab = (index) => {
     tabs[currentTargetIndex].children[0].classList.remove("--active");
@@ -33,6 +37,8 @@ const selectSearchTab = () => {
     tabActive.style.transform = `translate(calc(${translation}), -50%)`;
     tabActive.innerText = tabs[index].children[0].innerText;
     radio2Label.innerText = `${labelArr[index]} 검색`;
+    selectLabel.innerText = `${labelName[index]}`;
+    selectedOption.innerText = `${selected[index]}`;
   };
 
   tabs.forEach((tab, index) => {
@@ -43,15 +49,93 @@ const selectSearchTab = () => {
     });
   });
 
+  const optionUl = document.querySelector(".option");
+
+  const startNumber = 13;
+  const endNumber = 22;
+  const inchData = Array.from(
+    { length: endNumber - startNumber + 1 },
+    (_, index) => startNumber + index
+  );
+  const batteryData = [
+    "전체",
+    "DF40R",
+    "DF40L",
+    "DF40AL",
+    "DIN40L (54018)",
+    "DIN50L",
+    "DF50L",
+    "DIN60HL (55530)",
+    "DF60R",
+    "DF60L",
+    "DIN60L",
+    "DIN74L (57412)",
+    "DIN74R",
+    "DF80R",
+    "DF80L",
+    "DIN80L",
+    "DIN90L (59043)",
+    "DF90R",
+    "DF90L",
+    "DIN90R (59095)",
+    "DF100R",
+    "DIN100L (60038)",
+    "DF100L",
+    "DIN115L (61544)",
+  ];
+  const brandData = [
+    "전체",
+    "현대모비스",
+    "르노코리아자동차",
+    "캐스트롤",
+    "KG모빌리티",
+  ];
+
   radio1[0].addEventListener("click", () => {
-    console.log("1");
     contentSelects[1].classList.remove("--active");
     contentSelects[0].classList.add("--active");
   });
   radio2[0].addEventListener("click", () => {
-    console.log("2");
     contentSelects[0].classList.remove("--active");
     contentSelects[1].classList.add("--active");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (e.target.closest("div")?.classList[0] == "custom-select") {
+      changeSelected();
+
+      [...optionUl.children].forEach((item) => {
+        item.remove();
+      });
+
+      let inputIdx = labelName.indexOf(selectLabel.innerText);
+
+      if (inputIdx === 0) {
+        inchData.forEach((data) => {
+          optionUl.insertAdjacentHTML(
+            "beforeend",
+            `<li class="option-item">${data}</li>`
+          );
+        });
+      } else if (inputIdx === 1) {
+        batteryData.forEach((data) => {
+          optionUl.insertAdjacentHTML(
+            "beforeend",
+            `<li class="option-item">${data}</li>`
+          );
+        });
+      } else {
+        brandData.forEach((data) => {
+          optionUl.insertAdjacentHTML(
+            "beforeend",
+            `<li class="option-item">${data}</li>`
+          );
+        });
+      }
+    }
+    if (e.target.closest("div")?.classList[0] !== "custom-select") {
+      selectedOption.parentNode.classList.remove("--active");
+    }
   });
 };
 const searchCarBtn = () => {
@@ -71,6 +155,26 @@ const searchCarBtn = () => {
 
       btn.disabled = !allFilled;
     });
+  });
+};
+
+const changeSelected = () => {
+  const label = document.querySelector(".label");
+  const options = document.querySelectorAll(".option-item");
+
+  if (options.length > 0) {
+    document.addEventListener("click", (e) => {
+      if (e.target.className === "option-item")
+        label.innerHTML = e.target.innerText;
+    });
+  }
+
+  label.addEventListener("click", () => {
+    if (label.parentNode.classList.contains("--active")) {
+      label.parentNode.classList.remove("--active");
+    } else {
+      label.parentNode.classList.add("--active");
+    }
   });
 };
 
