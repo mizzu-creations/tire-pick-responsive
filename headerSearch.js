d308621179addbbc32b05a728ed4e9bd64d0c171
mizzu-creations <mizzu.creations@gmail.com>
2024-02-28 16:24:45 +0900

    Feat : 검색 input radio (1)

--- headerSearch.js
+++ headerSearch.js
@@ -2,20 +2,26 @@ const openSearchBox = () => {
   const searchBtn = document.querySelector(".search");
   const searchContent = document.querySelector(".search__contents");
 
-  searchBtn.addEventListener("click", () => {
+  searchBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
     searchContent.open = !searchContent.open;
   });
-};
 
+  document.addEventListener("click", (e) => {
+    if (!e.target.closest("dialog")) {
+      searchContent.open = false;
+    }
+  });
+};
 const selectSearchTab = () => {
   const tabs = document.querySelectorAll(".search__tab");
-  const contents = document.querySelector(".search__content");
   const tabActive = document.querySelector(".search__tab-focus");
   let currentTargetIndex = 0;
 
   const radio1 = document.querySelectorAll("#searchChoice1");
   const radio2 = document.querySelectorAll("#searchChoice2");
   const radio2Label = document.querySelector(`label[for='searchChoice2']`);
+  const contentSelects = document.querySelectorAll(".search__content__select");
 
   const labelArr = ["사이즈로", "규격", "브랜드"];
 
@@ -36,7 +42,38 @@ const selectSearchTab = () => {
       }
     });
   });
+
+  radio1[0].addEventListener("click", () => {
+    console.log("1");
+    contentSelects[1].classList.remove("--active");
+    contentSelects[0].classList.add("--active");
+  });
+  radio2[0].addEventListener("click", () => {
+    console.log("2");
+    contentSelects[0].classList.remove("--active");
+    contentSelects[1].classList.add("--active");
+  });
+};
+const searchCarBtn = () => {
+  const inputs = document.querySelectorAll(".search__item1 input");
+  const btn = document.querySelector(".search__item1 button");
+
+  inputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      let allFilled = true; // 추후 정규식 validation 필요
+
+      inputs.forEach((input) => {
+        if (input.value === "") {
+          allFilled = false;
+          console.log(allFilled);
+        }
+      });
+
+      btn.disabled = !allFilled;
+    });
+  });
 };
 
 openSearchBox();
 selectSearchTab();
+searchCarBtn();
