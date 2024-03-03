21242f00e2e07095bef34735a71fd6d8e431f9a8
mizzu-creations <mizzu.creations@gmail.com>
2024-03-04 00:35:56 +0900

    Feat : 메인 메뉴 레이아웃 완성 (2) - 모바일
    - flex를 grid로 전환하여 메뉴 반응형 적용
    - 가로 사이즈와 폰트 사이즈에 따라 레이아웃 깨짐이 발생하므로, 미디어 쿼리를 2단계로 나누어 설정
    - 메인 배너 슬라이드 이미지 웹, 모바일에 따라 변경

--- js/mainBanner.js
+++ js/mainBanner.js
@@ -1,62 +1,84 @@
 const bannerImgs = [
   {
-    img: "./images/slide_main01.webp",
+    img: "./images/slide_main01",
     color: "#d3e3df",
     alt: "주말마다 만나요! 타이어픽 만포인트 참여 시 100% 드려요",
   },
   {
-    img: "./images/slide_main02.webp",
-    color: "#101211",
-    alt: "내가 제일 잘 나가! 수입차 오너의 픽 반응좋은 타이어 모음",
-  },
-  {
-    img: "./images/slide_main03.webp",
+    img: "./images/slide_main02",
     color: "#ecf2fb",
     alt: "삼공사공 여성의 픽 인기 타이어 탑 텐 최대 53% 할인",
   },
   {
-    img: "./images/slide_main04.webp",
+    img: "./images/slide_main03",
     color: "#2b5cd2",
     alt: "와이퍼도 타이어픽 전 상품 무료배송 최대 74% 할인",
   },
   {
-    img: "./images/slide_main05.webp",
+    img: "./images/slide_main04",
     color: "#6f49fa",
     alt: "구매리뷰 쓰고 만사천 포인트 받자! 타이어픽 리뷰 이벤트",
   },
   {
-    img: "./images/slide_main06.webp",
+    img: "./images/slide_main05",
     color: "#21273c",
     alt: "신규회원 3천원 할인 앱 설치하면 4천원 더",
   },
   {
-    img: "./images/slide_main07.webp",
+    img: "./images/slide_main06",
     color: "#070707",
     alt: "나도 친구도 만원씩 사이좋게 혜택받자 친구초대 이벤트",
   },
   {
-    img: "./images/slide_main08.webp",
+    img: "./images/slide_main07",
     color: "#3264d3",
     alt: "집 앞에서 간편교체! 출장교체 지역 확대 서울부터 경기 성남시까지",
   },
 ];
+let imgChangeFlag = false;
 
 const createSlide = () => {
   const swiperWrapper = document.querySelector(".swiper-wrapper");
-
   bannerImgs.forEach((data) => {
     swiperWrapper.insertAdjacentHTML(
       "beforeend",
       `<div class="swiper-slide" style="background-color: ${data.color}">
 <div class="slide-bg">
-  <img src="${data.img}" alt="${data.alt}" />
+  <img src="${data.img}.webp" alt="${data.alt}" />
 </div>
 </div>`
     );
   });
 };
+const changeMobileImg = () => {
+  const swiperSlide = document.querySelectorAll(".swiper-slide");
+
+  if (window.innerWidth <= 769 && !imgChangeFlag) {
+    swiperSlide.forEach((slide) => {
+      const slideBg = slide.children;
+      let slideImg = slideBg[0].children[0];
+      let src = slideImg.src;
+      src = src.replace(".webp", "_m.webp");
+      slideImg.src = src;
+    });
+
+    imgChangeFlag = true;
+  } else if (window.innerWidth > 769 && imgChangeFlag) {
+    swiperSlide.forEach((slide) => {
+      const slideBg = slide.children;
+      let slideImg = slideBg[0].children[0];
+      let src = slideImg.src;
+      src = src.replace(/_m(?=.webp)/, "");
+      slideImg.src = src;
+    });
+
+    imgChangeFlag = false;
+  }
+};
 
 createSlide();
+changeMobileImg();
+window.addEventListener("resize", changeMobileImg);
 
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
@@ -73,7 +95,6 @@ const swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-
 const swiperBtnHover = () => {
   const swiper = document.querySelector(".swiper");
   const next = document.querySelector(".swiper-button-next");
@@ -88,7 +109,6 @@ const swiperBtnHover = () => {
     prev.style.display = "none";
   });
 };
-
 const responsiveApplication = () => {
   const swiperFraction = document.querySelector(".swiper-pagination-fraction");
 
