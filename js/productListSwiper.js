7773e03f6f4ae49cdf3ee3fb231b09c2e615bec0
mizzu-creations <mizzu.creations@gmail.com>
2024-03-12 13:52:21 +0900

    Feat : 제품 리스트 슬라이드 구현 (2) - 모바일

--- js/productListSwiper.js
+++ js/productListSwiper.js
@@ -1,15 +1,31 @@
+const breakWidth = {
+  490: {
+    slidesPerView: 1.3,
+    spaceBetween: 0,
+  },
+  590: {
+    slidesPerView: 1.7,
+    spaceBetween: 0,
+  },
+  690: {
+    slidesPerView: 2.1,
+    spaceBetween: 0,
+  },
+  769: {
+    slidesPerView: 4,
+    spaceBetween: 24,
+  },
+};
+
 const productListswiper = new Swiper(".swiper.product-list", {
-  slidesPerView: 2.2,
+  slidesPerView: 1.07,
   spaceBetween: 0,
   pagination: {
     el: ".swiper-pagination",
     clickable: false,
   },
   breakpoints: {
-    769: {
-      slidesPerView: 4,
-      spaceBetween: 24,
-    },
+    ...breakWidth,
   },
   navigation: {
     nextEl: ".swiper-button-next.product-list",
