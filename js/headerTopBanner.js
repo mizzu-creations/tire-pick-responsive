054155945704c2f7ba9e3a5b9034d1f1ff2ea2e0
mizzu-creations <mizzu.creations@gmail.com>
2024-03-12 18:43:58 +0900

    Feat : 상단 배너 컴포넌트 구현 (2) - 모바일

--- js/headerTopBanner.js
+++ js/headerTopBanner.js
@@ -1,8 +1,25 @@
-const topBanner = document.querySelector(".top-banner");
-const closeBtn = topBanner.querySelector(".top-banner > button");
+const closeTopBanner = () => {
+  const topBanner = document.querySelector(".top-banner");
+  const closeBtn = document.querySelector(".top-banner > button");
 
-closeBtn.addEventListener("click", () => {
-  topBanner.style.visibility = "hidden";
-  topBanner.style.height = "0";
-  topBanner.style.padding = "0";
-});
+  closeBtn.addEventListener("click", () => {
+    topBanner.style.visibility = "hidden";
+    topBanner.style.height = "0";
+    topBanner.style.padding = "0";
+  });
+};
+const changeInnerText = () => {
+  const topBannerTxt = document.querySelector(".top-banner > div > span");
+
+  if (window.innerWidth <= 769) {
+    topBannerTxt.innerText = "보쉬, 미쉐린 와이퍼 무료배송!";
+  }
+  if (window.innerWidth > 769) {
+    topBannerTxt.innerText = "타이어픽 앱 설치하면 세차 100원";
+  }
+};
+
+closeTopBanner();
+changeInnerText();
+
+window.addEventListener("resize", changeInnerText);
