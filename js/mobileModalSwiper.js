ae0344cc98caf5941ab8d3951e71192a4fa80376
mizzu-creations <mizzu.creations@gmail.com>
2024-03-13 12:50:24 +0900

    Refactor : mobileModalSwiper 중복 코드 리팩토링

--- js/mobileModalSwiper.js
+++ js/mobileModalSwiper.js
@@ -5,36 +5,40 @@ const blockBodyScroll = () => {
   const body = document.querySelector("body");
   if (window.innerWidth < 769) {
     body.style.overflow = mobileModalBox.open ? "hidden" : "auto";
-  }
-  if (window.innerWidth >= 769) {
+  } else {
     body.style.overflow = "auto";
   }
 };
+const handleCloseModal = () => {
+  mobileModalBackdrop.style.display = "none";
+  mobileModalBox.open = false;
+  blockBodyScroll();
+};
 const closeModal = () => {
   const modalBtns = document.querySelectorAll(".mobile-modal__btns");
-
-  const handleCloseModal = () => {
-    mobileModalBackdrop.style.display = "none";
-    mobileModalBox.open = false;
-    blockBodyScroll();
-  };
-
   modalBtns.forEach((btn) => {
     btn.addEventListener("click", handleCloseModal);
   });
 };
 const handleBackdropClick = (e) => {
   if (e.target.classList.contains("mobile-modal")) {
-    mobileModalBackdrop.style.display = "none";
-    mobileModalBox.open = false;
-    blockBodyScroll();
+    handleCloseModal();
   }
 };
 
 blockBodyScroll();
 closeModal();
+
 window.addEventListener("click", handleBackdropClick);
-window.addEventListener("resize", blockBodyScroll);
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 769) {
+    mobileModalBackdrop.style.display = "block";
+    mobileModalBox.open = true;
+    blockBodyScroll();
+  } else {
+    handleCloseModal();
+  }
+});
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     mobileModalBox.classList.add("show");
