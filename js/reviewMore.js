0ba7754e99e9fd8e1af6b68d41b08a42034d531a
mizzu-creations <mizzu.creations@gmail.com>
2024-04-02 15:27:52 +0900

    Add : 리뷰 타입 별 카드 추가

--- js/reviewMore.js
+++ js/reviewMore.js
@@ -1,6 +1,12 @@
 const moreBtns = document.querySelectorAll(".review-more");
 const customerReview = document.querySelectorAll(".customer-review");
 
+customerReview.forEach((review, idx) => {
+  if (review.children.length < 7) {
+    moreBtns[idx].style.display = "none";
+  }
+});
+
 moreBtns.forEach((btn, idx) => {
   btn.addEventListener("click", () => {
     customerReview[idx].classList.toggle("--active");
