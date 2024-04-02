227a8765108da38124ad56ad9323b5b60c95ef92
mizzu-creations <mizzu.creations@gmail.com>
2024-04-02 21:04:17 +0900

    Feat : 메인 미니 배너 슬라이드 구현

---
+++ js/mainMiniBannerSwiper.js
@@ -0,0 +1,15 @@
+const miniBannerSwiper = new Swiper(".swiper.mini", {
+  slidesPerView: "auto",
+  spaceBetween: -30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    769: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+});
