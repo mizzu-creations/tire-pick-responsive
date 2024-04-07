06da467409694959625f11eced1f8408fa0936c3
mizzu-creations <mizzu.creations@gmail.com>
2024-04-07 13:12:49 +0900

    Update : product list data - (1)

---
+++ js/productListCardTemplate.js
@@ -0,0 +1,112 @@
+import { addComma } from "./utils.js";
+
+const basicProductCard = (data) => {
+  return `<div class="swiper-slide">
+  <a href="javascript:void(0)">
+    <figure class="product">
+      <div class="product__img">
+      ${
+        data.rank
+          ? `<span class="product__img__rank">${data.rank}위</span>`
+          : data.iwol
+          ? `<span class="product__img__rank iwol">이월상품</span>`
+          : data.new
+          ? `<span class="product__img__rank new">NEW</span>`
+          : ""
+      }
+        <img src="${data.tireImg}" alt="" class="product__img__tire">
+        <img src="${data.brandImg}" alt="브랜드" class="product__img__mark">
+      </div>
+      <figcaption class="product__info">
+        <div class="product__info__brand"><span class="hide">브랜드</span>${
+          data.brand
+        }</div>
+        <div class="product__info__title"><span class="hide">제품명</span>${
+          data.title
+        }</div>
+        <div class="product__info__model"><span class="hide">모델명</span>${
+          data.model
+        }</div>
+        <div class="product__info__price">
+          <span class="hide">할인율</span>
+          <span class="product__info__price__discount">${data.discount}%</span>
+          <span class="hide">가격</span>
+          <span class="product__info__price__value">${addComma(
+            data.price
+          )}원</span>
+        </div>
+        ${
+          data.review
+            ? `<div class="product__info__review">
+        <span class="hide">별점</span>
+        <span class="product__info__review__rate">${data.rate}</span>
+        <span class="hide">리뷰</span>
+        <span class="product__info__review__count">리뷰 ${addComma(
+          data.review
+        )}</span>
+      </div>`
+            : ""
+        }
+        <ul class="product__info__keywords">
+        ${Object.values(data.keywords)
+          .map((keyword) => {
+            const isOutOfStock = keyword.split(" ")[0] === "품절임박";
+            const liClass = isOutOfStock ? "stock" : "";
+            // const liContent = isOutOfStock ? keyword : `<li>${keyword}</li>`;
+            return `<li class="${liClass}">${keyword}</li>`;
+          })
+          .join("")}
+      </ul>
+      ${
+        data.launchEvent
+          ? `<div class="product__info__event">런칭기념 만원 쿠폰 자동적용</div>`
+          : ""
+      }
+      </figcaption>
+    </figure>
+  </a>
+</div>`;
+};
+const risingProductCard = (data) => {
+  return `<div class="swiper-slide sample">
+  <a href="javascript:void(0)">
+    <figure class="product rising-product">
+      <div class="product__img">
+        <img
+          src="${data.productImg}"
+          alt=""
+          class="product__img__tire"
+        />
+        <img
+          src="${data.brandImg}"
+          alt="브랜드"
+          class="product__img__mark"
+        />
+      </div>
+      <figcaption class="product__info rising-product">
+        <div class="product__info__brand">
+          <span class="hide">브랜드</span>${data.brand}
+        </div>
+        <div class="product__info__title">
+          <span class="hide">제품명</span>${data.title}
+        </div>
+        <div class="product__info__price">
+          <span class="hide">할인율</span>
+          <span class="product__info__price__discount"
+            >최저가</span
+          >
+          <span class="hide">가격</span>
+          <span class="product__info__price__value"
+            >${addComma(data.price)}원 ~</span
+          >
+        </div>
+        <div class="product__info__description rising-product">
+          ${data.description}
+        </div>
+      </figcaption>
+    </figure>
+  </a>
+</div>`;
+};
+
+export { basicProductCard, risingProductCard };
--- js/productListSwiper.js
+++ js/productListSwiper.js
@@ -1,207 +1,37 @@
-const imgSrc = "./assets/images/webp/";
-const productList = [
-  {
-    rank: 1,
-    tireImg: `${imgSrc}tire/tire_kh_hp71_11_45degree.webp`,
-    brandImg: `${imgSrc}logo/logo_kumho.webp`,
-    brand: "금호",
-    title: "크루젠 HP71",
-    model: "235/55R19",
-    discount: 47,
-    price: 125000,
-    rate: 4.7,
-    review: 2833,
-    keywords: { 1: "사계절용", 2: "SUV", 3: "고급형" },
-  },
-  {
-    rank: 2,
-    tireImg: `${imgSrc}tire/tire_kh_ta91_1_45degree.webp`,
-    brandImg: `${imgSrc}logo/logo_kumho.webp`,
-    brand: "금호",
-    title: "마제스티9 솔루스 TA91",
-    model: "245/45R18",
-    discount: 46,
-    price: 143500,
-    rate: 4.8,
-    review: 2288,
-    keywords: { 1: "사계절용", 2: "승용차", 3: "고급형" },
-  },
-  {
-    rank: 3,
-    tireImg: `${imgSrc}tire/tire_hk_ventusS2AsH462_1_45degree.webp`,
-    brandImg: `${imgSrc}logo/logo_hankook.webp`,
-    brand: "한국타이어",
-    title: "벤투스 S2 AS",
-    model: "245/45R18",
-    discount: 42,
-    price: 158100,
-    rate: 4.8,
-    review: 624,
-    keywords: { 1: "사계절용", 2: "승용차", 3: "고급형" },
-  },
-  {
-    rank: 4,
-    tireImg: `${imgSrc}tire/tire_kh_hp71_11_45degree.webp`,
-    brandImg: `${imgSrc}logo/logo_kumho.webp`,
-    brand: "금호",
-    title: "크루젠 HP71",
-    model: "225/55R18",
-    discount: 47,
-    price: 100900,
-    rate: 4.7,
-    review: 2840,
-    keywords: { 1: "사계절용", 2: "SUV", 3: "고급형" },
-  },
-  {
-    rank: 5,
-    tireImg: `${imgSrc}tire/tire_kh_kl33_1_45degree.webp`,
-    brandImg: `${imgSrc}logo/logo_kumho.webp`,
-    brand: "금호",
-    title: "크루젠 프리미엄 KL33",
-    model: "235/60R18",
-    discount: 49,
-    price: 112000,
-    rate: 4.7,
-    review: 1547,
-    keywords: { 1: "사계절용", 2: "SUV", 3: "가성비" },
-  },
-  {
-    rank: 6,
-    tireImg: `${imgSrc}tire/tire_kh_ta91_1_45degree.webp`,
-    brandImg: `${imgSrc}logo/logo_kumho.webp`,
-    brand: "금호",
-    title: "마제스티9 솔루스 TA91",
-    model: "225/55R17",
-    discount: 45,
-    price: 126400,
-    rate: 4.8,
-    review: 2288,
-    keywords: { 1: "사계절용", 2: "승용차", 3: "고급형" },
-  },
-  {
-    rank: 7,
-    tireImg: `${imgSrc}tire/tire_kh_kl33_1_45degree.webp`,
-    brandImg: `${imgSrc}logo/logo_kumho.webp`,
-    brand: "금호",
-    title: "크루젠 프리미엄 KL33",
-    model: "235/55R19",
-    discount: 49,
-    price: 106000,
-    rate: 4.7,
-    review: 1547,
-    keywords: { 1: "사계절용", 2: "SUV", 3: "가성비" },
-  },
-  {
-    rank: 8,
-    tireImg: `${imgSrc}tire/tire_kh_hp71_11_45degree.webp`,
-    brandImg: `${imgSrc}logo/logo_kumho.webp`,
-    brand: "금호",
-    title: "크루젠 HP71",
-    model: "235/60R18",
-    discount: 46,
-    price: 135000,
-    rate: 4.7,
-    review: 2840,
-    keywords: { 1: "사계절용", 2: "SUV", 3: "고급형" },
-  },
-  {
-    rank: 9,
-    tireImg: `${imgSrc}tire/tire_ns_ah8_1_45degree.webp`,
-    brandImg: `${imgSrc}logo/logo_nexen.webp`,
-    brand: "넥센",
-    title: "엔프리즈 AH8",
-    model: "245/45R18",
-    discount: 50,
-    price: 91000,
-    rate: 4.7,
-    review: 1225,
-    keywords: { 1: "사계절용", 2: "승용차", 3: "가성비" },
-  },
-  {
-    rank: 10,
-    tireImg: `${imgSrc}tire/tire_kh_ta91_1_45degree.webp`,
-    brandImg: `${imgSrc}logo/logo_kumho.webp`,
-    brand: "금호",
-    title: "마제스티9 솔루스 TA91",
-    model: "235/45R18",
-    discount: 44,
-    price: 142500,
-    rate: 4.8,
-    review: 2288,
-    keywords: { 1: "사계절용", 2: "승용차", 3: "고급형" },
-  },
-];
-
-const addComma = (number) => {
-  return number.toLocaleString();
-};
+import {
+  bestSeller,
+  lowestPrice,
+  risingProduct,
+  winter,
+} from "./productListData.js";
+import {
+  basicProductCard,
+  risingProductCard,
+} from "./productListCardTemplate.js";
 
 const swiperSectons = document.querySelectorAll(".product-list .swiper");
 
 const createProductCard = () => {
   swiperSectons.forEach((section) => {
     const wrapper = section.querySelector(".swiper-wrapper");
-    productList.forEach((data) => {
-      if (!section.classList.contains("rising-product")) {
-        wrapper.insertAdjacentHTML(
-          "beforeend",
-          `<div class="swiper-slide">
-        <a href="javascript:void(0)">
-          <figure class="product">
-            <div class="product__img">
-              <span class="product__img__rank">${data.rank}위</span>
-              <img src="${data.tireImg}" alt="" class="product__img__tire">
-              <img src="${
-                data.brandImg
-              }" alt="브랜드" class="product__img__mark">
-            </div>
-            <figcaption class="product__info">
-              <div class="product__info__brand"><span class="hide">브랜드</span>${
-                data.brand
-              }</div>
-              <div class="product__info__title"><span class="hide">제품명</span>${
-                data.title
-              }</div>
-              <div class="product__info__model"><span class="hide">모델명</span>${
-                data.model
-              }</div>
-              <div class="product__info__price">
-                <span class="hide">할인율</span>
-                <span class="product__info__price__discount">${
-                  data.discount
-                }%</span>
-                <span class="hide">가격</span>
-                <span class="product__info__price__value">${addComma(
-                  data.price
-                )}원</span>
-              </div>
-              <div class="product__info__review">
-                <span class="hide">별점</span>
-                <span class="product__info__review__rate">${data.rate}</span>
-                <span class="hide">리뷰</span>
-                <span class="product__info__review__count">리뷰 ${addComma(
-                  data.review
-                )}</span>
-              </div>
-              <ul class="product__info__keywords">
-              ${Object.values(data.keywords)
-                .map((keyword) => {
-                  const outOfStock = keyword.split(" ")[0];
-                  if (outOfStock === "품절임박") {
-                    return `<li class="stock">${keyword}</li>`;
-                  } else {
-                    return `<li>${keyword}</li>`;
-                  }
-                })
-                .join("")}
-            </ul>
-            </figcaption>
-          </figure>
-        </a>
-      </div>`
-        );
-      }
-    });
+    if (section.classList.contains("lowest-price")) {
+      lowestPrice.forEach((data) => {
+        wrapper.insertAdjacentHTML("beforeend", basicProductCard(data));
+      });
+    } else if (section.classList.contains("winter")) {
+      winter.forEach((data) => {
+        wrapper.insertAdjacentHTML("beforeend", basicProductCard(data));
+      });
+    } else if (!section.classList.contains("rising-product")) {
+      bestSeller.forEach((data) => {
+        wrapper.insertAdjacentHTML("beforeend", basicProductCard(data));
+      });
+    } else {
+      risingProduct.forEach((data) => {
+        wrapper.insertAdjacentHTML("beforeend", risingProductCard(data));
+      });
+    }
+
     if (
       !section.classList.contains("best-seller") &&
       !section.classList.contains("rising-product")
@@ -209,8 +39,10 @@ const createProductCard = () => {
       wrapper.insertAdjacentHTML(
         "beforeend",
         `<div class="swiper-slide view-all">
-        <div></div>
-        전체보기
+        <a href="javascript:void(0)">
+          <div></div>
+          전체보기
+        </a>
       </div>`
       );
     }
@@ -271,7 +103,7 @@ swiperSectons.forEach((section) => {
     if (window.innerWidth <= 769 && uniqueClassName === "rising-product") {
       return;
     } else {
-      new Swiper(`.swiper.${uniqueClassName}`, {
+      const productSwiper = new Swiper(`.swiper.${uniqueClassName}`, {
         slidesPerView: "auto",
         spaceBetween: uniqueClassName === "best-seller" ? -40 : -24,
         pagination: {
@@ -289,6 +121,7 @@ swiperSectons.forEach((section) => {
           prevEl: `.swiper-button-prev.${uniqueClassName}`,
         },
       });
+      productSwiper.update();
     }
   });
 
